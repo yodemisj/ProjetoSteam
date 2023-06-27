2d04f5cdb6dbfe3980f99b21a42e229f50f263ef
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2023-06-26 21:52:19 -0300

    Criação de classes Jogo, Legenda, Linguagem, Dev

---
+++ src/main/java/Empresa/Distribuidor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Empresa;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Distribuidor extends Empresa {
+
+    public Distribuidor() {
+    }
+}
